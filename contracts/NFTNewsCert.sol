34fad3ebfaea4a69a6674b01c33bc189775dbfc0
haruxx <harukatarotaro@gmail.com>
2022-07-09 08:28:07 +0900

    update to simple way

--- contracts/NFTNewsCert.sol
+++ contracts/NFTNewsCert.sol
@@ -9,165 +9,105 @@ import "hardhat/console.sol";
 
 contract NFTNewsCert is ERC721, Ownable {
     uint256 private currentTokenId = 0;
-    string public message = "defailt message";
     bool sw = false;
-    uint256 nftAmount = 1000;
-    uint256 limit = 3;
-    bytes32 keyword;
+    uint256 totalNumer = 100;
+    uint256 limit = 1;
 
-    enum Team {Red, Blue}
-    mapping(Team => string) teamString;
-    mapping(Team => uint256) teamScore;
-    Team lastTeam = Team.Red;
+    enum Color {Black, Red, Blue}
+    mapping(Color => string) colorString;
 
-    mapping(address => Team) myTeam;
-    mapping(address => uint256) myNFTs;
-    mapping(address => bool) usedBonus;
-    mapping(uint256 => Team) tokenTeam;
-    event Mint(address indexed minter, string indexed team,string indexed message, uint256 scoreRed, uint256 scoreBlue);
+    mapping(uint256 => Color) tokenColor;
+    mapping(uint256 => address) tokenOwner;
+    mapping(uint256 => string) tokenSignature;
+    mapping(address => uint256) numOfMinted;
+
+    event Mint(address indexed minter, string indexed color,string signature);
     constructor(
         string memory _name,
         string memory _symbol
     ) ERC721(_name, _symbol) {
-        teamString[Team.Red] = "Red";
-        teamString[Team.Blue] = "Blue";
-        teamScore[Team.Red] = 1;
-        teamScore[Team.Blue] = 1;
-        setkeyword("shibuya");
+        colorString[Color.Black] = "black";
+        colorString[Color.Red] = "red";
+        colorString[Color.Blue] = "blue";
+    }
+    function getNumberOfMinted(address _address) public view returns (uint256) {
+        return numOfMinted[_address];
     }
 
-    //Utility
-    function circleSize(Team _team) internal view returns (uint256) {
-        uint256 totalScore = teamScore[Team.Red] + teamScore[Team.Blue];
-
-        if(totalScore < 15 ){
-            return teamScore[_team] * 10;
-        }
-        else{
-            return  150 * teamScore[_team] / totalScore;
-        }
+    function mintRed( string memory yourName) public{
+        mintTo( Color.Red, yourName);
     }
-        function circleXposition(Team _team) internal view returns (uint256) {
-        uint256 totalScore = teamScore[Team.Red] + teamScore[Team.Blue];
-        
-        uint256 ret = 0;
-        if(totalScore < 30 ){
-            if(_team == Team.Red){ ret = 75;}
-            else if(_team == Team.Blue) { ret = 225;}
-        }
-        else{
-            if(_team == Team.Red){
-                ret = circleSize(Team.Red);
-            }
-            else if(_team == Team.Blue) {
-                ret = circleSize(Team.Red) * 2+ circleSize(Team.Blue);}
-        }
-        return ret;
+    function mintBlue(string memory yourName) public{
+        mintTo(Color.Blue, yourName);
     }
-
-    //external function
-    function getTeamScoreRed() public view returns (uint256) {
-        return teamScore[Team.Red];
-    }
-    function getTeamScoreBlue() public view returns (uint256) {
-        return teamScore[Team.Blue];
-    }
-    function getNumberOfNfts(address _address) public view returns (uint256) {
-        return myNFTs[_address];
-    }
-
-    function mintRed(string memory _keyword, string memory _message) public{
-        mintTo(_keyword, _message, Team.Red);
-    }
-    function mintBlue(string memory _keyword, string memory _message) public{
-        mintTo(_keyword, _message, Team.Blue);
-    }
-    function mintTo(string memory _keyword, string memory _message, Team _team) private{
+    function mintTo(Color _color,string memory _name) private{
         address _to = msg.sender;
-        require(bytes(_message).length <= 140, "Message is too long");
-        require(currentTokenId < nftAmount -1, "Token amount is full)");
-        require(keyword == keccak256(abi.encodePacked(_keyword)), "Keyword is not correct");
-        require(myNFTs[_to] < limit, "You have too many NFTs");
+        require(currentTokenId < totalNumer, "Token amount is full)");
+        require(numOfMinted[_to] < limit, "You reached mint limite");
         uint256 newTokenId = _getNextTokenId();
         _mint(_to, newTokenId);
         
-        message = _message;
-        teamScore[_team] += 1;
-        lastTeam = _team;
-        tokenTeam[newTokenId] = _team;
-        myNFTs[_to] += 1;
+        tokenSignature[newTokenId] = _name;
+        tokenColor[newTokenId] = _color;        
 
-        emit Mint(msg.sender, teamString[_team], message, teamScore[Team.Red], teamScore[Team.Blue]);
+        emit Mint(msg.sender, colorString[_color], _name);
         _incrementTokenId();
     }
-
     function _getNextTokenId() private view returns (uint256) {
         return currentTokenId+1;
     }
     function _incrementTokenId() private {
         currentTokenId++;
     }
-    function tokenURI(uint256 tokenId) override public view returns (string memory) {
-        require(0 < tokenId && tokenId <= currentTokenId , "tokenId must be exist");
+        function isTokenExist(uint256 _tokenId) public view returns (bool) {
+        if(_tokenId < 1 || currentTokenId < _tokenId){return false;}
+        return tokenColor[_tokenId] != Color.Black;
+    }
+    function tokenURI(uint256 _tokenId) override public view returns (string memory) {
+        require(isTokenExist(_tokenId), "tokenId must be exist");
         string[4] memory metaData;
 
-        //flare parameter create
-        uint256 redSize = circleSize(Team.Red);
-        uint256 blueSize = circleSize(Team.Blue);
-
-        string memory redFlareColor = tokenTeam[tokenId] == Team.Red ? "fff" : "333";
-        string memory blueFlareColor = tokenTeam[tokenId] == Team.Blue ? "fff" : "333";
-
-        string memory svg; //block to avoid Stack too deep, try removing local variables. error
+        string memory svg; //block to avoid "Stack too deep error"
         {
-            string[29] memory p;
+            string[17] memory p;
             p[0] = '<svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMinYMin meet" viewBox="0 0 320 320"><style>.base { fill: white; font-family: serif; font-size: 14px;}</style>';
-            p[1] = '<rect width="100%" height="100%" fill="black"/><defs><filter id="f1"><feGaussianBlur in="SourceGraphic" stdDeviation="3" /></filter>';
-            p[2] = '<linearGradient id="R"><stop offset="0%" stop-color="red"/><stop offset="100%"/></linearGradient><linearGradient id="B"><stop offset="0%" stop-color="blue"/><stop offset="100%"/></linearGradient></defs>';
-
-            p[3] = string(abi.encodePacked(
-                '<circle cx="',
-                "75",
-                '" cy="160" r="',
-                Strings.toString(redSize + 4),
-                '" fill="#', redFlareColor,
-                '" filter="url(#f1)"/><circle cx="',
-                "75",
-                '" cy="160" r="',
-                Strings.toString(redSize),
-                '" fill="url(#R)"/>'));
-
+            p[1] = '<rect width="100%" height="100%" fill="black"/>';
+            p[2] = '<defs><filter id="f"><feGaussianBlur in="SourceGraphic" stdDeviation="3" /></filter><linearGradient id="r"><stop offset="0%" stop-color="red"/><stop offset="100%"/></linearGradient><linearGradient id="b"><stop offset="0%" stop-color="blue"/><stop offset="100%"/></linearGradient>';
+            p[3] = '<circle id="F" cx="0" cy="0" r="10" fill="#aaa" filter="url(#f)"/><circle id="R" cx="0" cy="0" r="9" fill="url(#r)"/><circle id="B" cx="0" cy="0" r="9" fill="url(#b)"/> </defs>';
+            
             p[4] = string(abi.encodePacked(
-                '<circle cx="',
-                "225",
-                '" cy="160" r="',
-                Strings.toString(blueSize + 4),
-                '" fill="#', blueFlareColor,
-                '" filter="url(#f1)"/><circle cx="',
-                "225",
-                '" cy="160" r="',
-                Strings.toString(blueSize),
-                '" fill="url(#B)"/>'));
+                '<use href="#F" x="', 
+                Strings.toString(((_tokenId- 1) % 10) * 22 + 40),
+                '" y="',
+                Strings.toString(((_tokenId- 1) / 10) * 22 + 95),
+                '"/>'
+            ));
+            
+            for(uint256 i = 0; i < 100; i++){
+                string memory head;
+                if(tokenColor[_tokenId] == Color.Black){continue;}
+                if(tokenColor[_tokenId] == Color.Red){head = '<use href="#R" x="';}
+                else if(tokenColor[_tokenId] == Color.Blue){head = '<use href="#B" x="';}
 
-            p[5] = '<text x="30" y="20" class="base">NFT News Certification #75</text><text x="30" y="40" class="base"> ID: ';
-            p[6] = Strings.toString(tokenId);
-            p[7] = '</text>';
-
-            p[8] = '<text x="30" y="220" fill="';
-            p[9] =  teamString[lastTeam];
-            p[10] = '" font-family="serif" font-size="10px">';
-            p[11] = message;
-            p[12] = '</text><text x="30" y="240" fill="';
-            p[13] = teamString[lastTeam];
-            p[14] = '" font-family="serif" font-size="10px">';
-            p[15] = Strings.toHexString(uint256(uint160(ownerOf(currentTokenId))));
-            p[16] = '</text></svg>';
-            svg = string(abi.encodePacked(p[0], p[1], p[2], p[3], p[4], p[5], p[6], p[7], p[8], p[9]));
-            svg = string(abi.encodePacked(svg, p[10], p[11], p[12], p[13], p[14], p[15], p[16]));     
+                p[5] = string(abi.encodePacked(
+                    head,
+                    Strings.toString((i % 10) * 22 + 40),
+                    '" y="',
+                    Strings.toString((i / 10) * 22 + 95),
+                    '"/>'
+                ));
+            }
+            p[6] = '<text x="30" y="20" class="base">NFT News Certification #75</text><text x="30" y="40" class="base"> ID: ';
+            p[7] = Strings.toString(_tokenId);
+            p[8] = '</text><text x="30" y="60" class="base"> for  ';
+            p[9] = tokenSignature[_tokenId];
+            p[10] = '</text>';
+            p[11] = '</svg>';
+            svg = string(abi.encodePacked(p[0], p[1], p[2], p[3], p[4], p[5], p[6], p[7], p[8], p[9], p[10], p[11]));
         }
 
         metaData[0] = '{"name": "NFTNewsCert #';
-        metaData[1] = Strings.toString(tokenId);
+        metaData[1] = Strings.toString(_tokenId);
         metaData[2] = '", "description": "NFT News Reading Certification.",';
         metaData[3] = '"image": "data:image/svg+xml;base64,';
         string memory json = Base64.encode(bytes(string(abi.encodePacked(metaData[0], metaData[1], metaData[2], metaData[3], Base64.encode(bytes(svg)), '"}'))));
@@ -178,20 +118,7 @@ contract NFTNewsCert is ERC721, Ownable {
         sw = _sw;
     }
 
-    function setkeyword(string memory _keyword) onlyOwner() public {
-        keyword = keccak256(abi.encodePacked(_keyword));
-    }
-
     function setLimit(uint256 _limit) onlyOwner() public {
         limit = _limit;
-    }
-
-    function getBonus(uint256 _t, uint256 _s) public{
-        uint256 n = myNFTs[msg.sender];
-        require(usedBonus[msg.sender] == false, "You have already used bonus");
-        require((_s * n) % 3 == 1, "Bad s");
-        require(_t %11 == 0, "Bad t");
-        if(_t == 22){teamScore[Team.Red] += 1;}
-        if(_t == 33){teamScore[Team.Blue] += 1;}
     }
 }
--- test/NFTNewsCert-test.js
+++ test/NFTNewsCert-test.js
@@ -25,17 +25,16 @@ describe("NFTNewsCert contract", function () {
 
     it("Should mint a token with token ID 1 & 2 to account1", async function () {
       let address1 = account1.address;
-      await token721.connect(account1).mintRed("shibuya","hello");
+      await token721.connect(account1).mintRed("shibuya");
       expect(await token721.ownerOf(1)).to.equal(address1);
       expect(await token721.balanceOf(address1)).to.equal(1);      
     });
 
     it("Should output tokeURI", async function () {
       const address1=account1.address;
-      await token721.mintRed("shibuya", "hello");
-      await token721.mintRed("shibuya", "I love NFT! Love Love!");
-      let tokenURI = await token721.tokenURI(2);
-
+      await token721.mintRed("shibuya");
+      let tokenURI = await token721.tokenURI(1);
+      console.log("tokenURI",tokenURI);
       let metaData = Buffer.from(tokenURI.split(",")[1], 'base64').toString('ascii');
       metaData = JSON.parse(metaData);
       console.log("name:", metaData.name);
@@ -46,60 +45,6 @@ describe("NFTNewsCert contract", function () {
       fs.writeFileSync("test.svg", image);
     });
 
-    it("Should Blue is Top", async function () {
-      const address1=account1.address;
-      await token721.mintBlue("shibuya", "hello");
-      await token721.mintBlue("shibuya", "I love NFT! Love Love!");
-      let tokenURI = await token721.tokenURI(2);
 
-      let metaData = Buffer.from(tokenURI.split(",")[1], 'base64').toString('ascii');
-      metaData = JSON.parse(metaData);
-      console.log("name:", metaData.name);
-      console.log("description:", metaData.description);
-      let image = metaData.image.split(",")[1];
-      image = Buffer.from(image, 'base64').toString('ascii');
-      console.log("image:", image);
-      fs.writeFileSync("test2.svg", image);
-    });
-
-    it("Should circle size is over limit", async function () {
-      const address1 = account1.address;
-      await token721.setLimit(100);
-      for(let i=0;i<30;i++){
-        await token721.mintBlue("shibuya", "hello");
-      }      
-      let tokenURI = await token721.tokenURI(2);
-
-      let metaData = Buffer.from(tokenURI.split(",")[1], 'base64').toString('ascii');
-      metaData = JSON.parse(metaData);
-      console.log("name:", metaData.name);
-      console.log("description:", metaData.description);
-      let image = metaData.image.split(",")[1];
-      image = Buffer.from(image, 'base64').toString('ascii');
-      console.log("image:", image);
-      fs.writeFileSync("test3.svg", image);
-    });
-
-    it("Should point up with Mint", async function () {
-      let point = await token721.connect(account1).getTeamScoreRed();
-      expect(point).to.equal(1);
-      await token721.connect(account1).mintRed("shibuya", "hello");
-      point = await token721.connect(account1).getTeamScoreRed();
-      expect(point).to.equal(2);
-    });
-
-    it("Should point up with Bonus,", async function () {
-      await token721.connect(account1).mintRed("shibuya", "hello");
-      await token721.connect(account1).getBonus(22,1);
-      point = await token721.connect(account1).getTeamScoreRed();
-      expect(point).to.equal(3);
-    });
-
-    it("Should not point up with Bonus, if wrong input", async function () {
-      await token721.connect(account1).mintRed("shibuya", "hello");
-      await expect(
-        token721.connect(account1).getBonus(22,2)
-      ).to.be.revertedWith("Bad s");
-    });
   });
 });
